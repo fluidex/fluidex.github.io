d20457e275aef81e4db7d7239da9178f192d4ed3
HAOYUatHZ <haoyu@protonmail.com>
2021-05-21 20:29:31 +0800

    change output dir

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require(`path`);
+const fs = require("fs");
 
 const toKebabCase = (str) => {
   return str
@@ -219,3 +220,21 @@ exports.createSchemaCustomization = ({ actions }) => {
     }
   `);
 };
+
+exports.onPreInit = () => {
+  if (process.argv[2] === "build") {
+    fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
+    fs.renameSync(
+      path.join(__dirname, "public"),
+      path.join(__dirname, "public_dev")
+    )
+  }
+}
+
+exports.onPostBuild = () => {
+  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "docs"))
+  fs.renameSync(
+    path.join(__dirname, "public_dev"),
+    path.join(__dirname, "public")
+  )
+}
