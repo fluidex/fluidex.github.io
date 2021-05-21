408d8e658323eb65a5bc2312104eddb2ee5ecc75
HAOYUatHZ <haoyu@protonmail.com>
2021-05-21 23:54:15 +0800

    fix tg preview

--- src/components/seo.js
+++ src/components/seo.js
@@ -54,7 +54,7 @@ const SEO = ({ description, lang, meta, title }) => {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:creator`,
