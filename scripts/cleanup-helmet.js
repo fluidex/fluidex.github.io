a80f8d87a46eb1109f0249e7387b20985beb61bc
HAOYUatHZ <haoyu@protonmail.com>
2021-05-24 13:46:48 +0800

    clean up helmet

---
+++ scripts/cleanup-helmet.js
@@ -0,0 +1,18 @@
+const replace = require('replace-in-file')
+
+const replaceOptions = {
+  files: ['public/**/*.html'],
+  from: / data-react-helmet="true"/g,
+  to: '',
+}
+
+async function replaceHelmetAttrs() {
+  const results = await replace(replaceOptions)
+  const count = results.reduce((cnt, res) => cnt + (res.hasChanged ? 1 : 0), 0)
+  console.log(`replaceHelmetAttrs: ${count} files changed`)
+}
+
+const start = Date.now()
+replaceHelmetAttrs()
+  .then(() => console.log(`postbuild done in ${Date.now() - start}ms`))
+  .catch((err) => console.error(err))
